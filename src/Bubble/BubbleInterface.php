2daf960fe5c46620848aedcdd0535e3e88fa4d5a
Josh Swift <joshuaswift@gmail.com>
2014-12-14 15:15:22 -0500

    interface fix

--- src/Bubble/BubbleInterface.php
+++ src/Bubble/BubbleInterface.php
@@ -2,11 +2,11 @@
 namespace bubblr\Bubble {
     use bubblr\Spout\SpoutInterface;
     use observr\Event\EventInterface;
+    use observr\Event\EventAwareInterface;
     
-    interface BubbleInterface extends EventInterface {
+    interface BubbleInterface extends EventInterface, EventAwareInterface {
         public function getResult();
         public function setContext($context);
-        public function cancel();
         public function resume(SpoutInterface $spout);
         public function suspend(SpoutInterface $spout);
     }
